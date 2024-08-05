f7896647ed1fa882d33152fed0082af93531cfff
adinata15 <adinatatan99@gmail.com>
2024-08-05 16:30:33 +0800

    Added in-place option

--- challenge/custom_sed/sed.cpp
+++ challenge/custom_sed/sed.cpp
@@ -19,6 +19,7 @@ enum OPTIONS
 {
     IN_PLACE = 0,
     LINE_NUM,
+    ADD_SPACE,
     UNKNOWN_OPTION,
 };
 
@@ -33,6 +34,8 @@ enum ERROR_TYPE
 {
     NO_ERROR = 0,
     PARSE_ERROR,
+    INPUT_ERROR,
+    FILE_ERROR,
     UNKNOWN_ERROR,
 };
 
@@ -40,7 +43,7 @@ std::vector<std::string> read_txt(const std::string &filepath)
 {
     std::vector<std::string> data;
     std::string line;
-    std::fstream file = std::fstream(filepath, std::ios::in | std::ios::out);
+    std::fstream file(filepath, std::ios::out | std::ios::in);
 
     if (file.is_open())
     {
@@ -55,10 +58,34 @@ std::vector<std::string> read_txt(const std::string &filepath)
     return data;
 }
 
+ERROR_TYPE write_txt(const std::string &filepath, const std::vector<std::string> &data)
+{
+    std::string line;
+    std::fstream file(filepath, std::ios::in | std::ios::out | std::ios::trunc); // trunc to complete overwrite file
+
+    if (file.is_open())
+    {
+        for (const std::string &line : data)
+        {
+            // std::cout << line << std::endl;
+            file << line;
+            file << std::endl;
+        }
+    }
+    else
+    {
+        return FILE_ERROR;
+    }
+
+    file.close();
+    return NO_ERROR;
+}
+
 void substitute(std::vector<std::string> &data, const std::string &target, const std::string &replacement, bool is_global)
 {
-    for (std::string &line : data)
+    for (size_t i = 0; i < data.size(); ++i)
     {
+        std::string &line = data[i];
         std::regex r_target(target);
         if (is_global)
             line = std::regex_replace(line, r_target, replacement, std::regex_constants::match_any);
@@ -67,11 +94,11 @@ void substitute(std::vector<std::string> &data, const std::string &target, const
     }
 }
 
-ERROR_TYPE parsePattern(char *pattern, std::vector<std::string> &tokens)
+ERROR_TYPE parsePattern(const std::string &pattern, std::vector<std::string> &tokens)
 {
     std::string curr;
 
-    for (size_t i = 0; pattern[i] != NULL; ++i)
+    for (size_t i = 0; i < pattern.size(); ++i)
     {
         if (pattern[i] == '/')
         {
@@ -80,7 +107,7 @@ ERROR_TYPE parsePattern(char *pattern, std::vector<std::string> &tokens)
         }
         else if (pattern[i] == '\\')
         {
-            if (pattern[i + 1] == NULL)
+            if (i == pattern.size() - 1)
             {
                 return PARSE_ERROR;
             }
@@ -100,6 +127,31 @@ ERROR_TYPE parsePattern(char *pattern, std::vector<std::string> &tokens)
     return NO_ERROR;
 }
 
+ERROR_TYPE parseOption(int argc, char *argv[], std::unordered_map<OPTIONS, std::string> &options_map, int &idx)
+{
+    while (idx < argc && argv[idx][0] == '-')
+    {
+        if (argv[idx][1] == 'i')
+        {
+            options_map[IN_PLACE] = "i";
+        }
+        else if (argv[idx][1] == 'G')
+        {
+            options_map[ADD_SPACE] = "g";
+        }
+        else if (argv[idx][1] == 'n')
+        {
+            if (idx >= argc - 1)
+                return PARSE_ERROR;
+
+            idx++;
+            options_map[LINE_NUM] = argv[idx];
+        }
+        idx++;
+    }
+    return NO_ERROR;
+}
+
 ERROR_TYPE parseCommand(int argc, char *argv[], std::unordered_map<OPTIONS, std::string> &options_map, std::string &filepath, std::vector<std::string> &pattern_tokens)
 {
     if (std::string(argv[0]) != "./ccsed")
@@ -107,55 +159,100 @@ ERROR_TYPE parseCommand(int argc, char *argv[], std::unordered_map<OPTIONS, std:
         return PARSE_ERROR;
     }
 
-    OPTIONS prev_opt = UNKNOWN_OPTION;
-    for (int i = 1; i < argc; ++i)
+    int idx = 1;
+    if (parseOption(argc, argv, options_map, idx) != NO_ERROR)
     {
-        // last args assumed to be input file path
-        if (i == argc - 1)
-        {
-            filepath = argv[i];
-            continue;
-        }
+        return PARSE_ERROR;
+    }
 
-        if (prev_opt != UNKNOWN_OPTION)
+    if (idx < argc - 1)
+    {
+        if (NO_ERROR != parsePattern(argv[idx], pattern_tokens))
         {
-            options_map[prev_opt] = argv[i];
-            prev_opt = UNKNOWN_OPTION;
-            continue;
+            return PARSE_ERROR;
         }
+        idx++;
+    }
 
-        std::string input = std::string(argv[i]);
-        if (input[0] == '-')
+    if (idx != argc - 1)
+    {
+        return PARSE_ERROR;
+    }
+
+    filepath = argv[idx];
+
+    return NO_ERROR;
+}
+
+ERROR_TYPE filterData(std::vector<std::string> &data, size_t start, size_t end, const std::string &search_pattern)
+{
+    if (start <= 0 || end > data.size())
+    {
+        return INPUT_ERROR;
+    }
+    std::regex pattern(search_pattern);
+    std::vector<std::string> temp;
+    for (size_t i = start - 1; i <= end; ++i)
+    {
+        if (std::regex_search(data[i], pattern))
         {
-            std::string option = input.substr(1, input.length());
-            if (option == "n")
-            {
-                prev_opt = LINE_NUM;
-            }
-            else if (option == "i")
-            {
-                prev_opt = IN_PLACE;
-            }
-            else
-            {
-                prev_opt = UNKNOWN_OPTION;
-            }
-        }
-        else
-        {
-            if (NO_ERROR != parsePattern(argv[i], pattern_tokens))
-            {
-                return PARSE_ERROR;
-            }
+            temp.push_back(data[i]);
         }
     }
+    data = temp;
     return NO_ERROR;
 }
 
 void execute(std::unordered_map<OPTIONS, std::string> &options_map, std::string &filename, std::vector<std::string> &pattern_tokens, std::vector<std::string> &data)
 {
     data = read_txt(filename);
-    if (pattern_tokens[0] == "s")
+
+    size_t s_line = 1, e_line = data.size() - 1;
+    std::string search_pattern = ".*";
+    if (options_map.find(LINE_NUM) != options_map.end())
+    {
+        std::cout << "Line options:" << std::endl;
+        std::cout << options_map[LINE_NUM] << std::endl;
+
+        if (options_map[LINE_NUM][0] == '/')
+        {
+            std::string curr;
+            for (char c : options_map[LINE_NUM])
+            {
+                if (c == '/')
+                {
+                    search_pattern = curr;
+                    curr.clear();
+                    continue;
+                }
+                curr += c;
+            }
+            if (curr == "p")
+            {
+            }
+        }
+        else
+        {
+            std::string curr;
+            for (char c : options_map[LINE_NUM])
+            {
+                if (c == ',')
+                {
+                    s_line = std::stoi(curr);
+                    curr.clear();
+                }
+                else if (c <= '9' && c >= '0')
+                {
+                    curr += c;
+                }
+            }
+            e_line = std::stoi(curr);
+        }
+    }
+
+    filterData(data, s_line, e_line, search_pattern);
+
+    if (pattern_tokens.size() && pattern_tokens[0] == "s")
     {
         substitute(data, pattern_tokens[1], pattern_tokens[2], pattern_tokens.size() > 3 && pattern_tokens[3] == "g");
     }
@@ -163,9 +260,10 @@ void execute(std::unordered_map<OPTIONS, std::string> &options_map, std::string 
 
 void print_output(std::vector<std::string> &data)
 {
-    for (std::string &d : data)
+    // , const std::vector<bool>& relevant_data)
+    for (size_t i = 0; i < data.size(); ++i)
     {
-        std::cout << d << std::endl;
+        std::cout << data[i] << std::endl;
     }
 }
 
@@ -174,6 +272,7 @@ int main(int argc, char *argv[])
     std::string filename;
     std::unordered_map<OPTIONS, std::string> options_map;
     std::vector<std::string> pattern_tokens, data;
+    // std::vector<bool> relavant_data;
 
     if (NO_ERROR != parseCommand(argc, argv, options_map, filename, pattern_tokens))
     {
@@ -190,8 +289,16 @@ int main(int argc, char *argv[])
     // std::cout << std::endl;
 
     execute(options_map, filename, pattern_tokens, data);
-    
-    print_output(data);
+
+    if (options_map.find(IN_PLACE) != options_map.end())
+    {
+        std::cout << "in place !!!" << std::endl;
+        write_txt(filename, data);
+    }
+    else
+    {
+        print_output(data);
+    }
 
     return 0;
 }
